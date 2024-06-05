3edc2cb664c128e50fd7c7e179fe82d2944f178b
Chris Cadek <cc@wh-i.at>
2024-06-05 13:17:17 +0200

    Ensure that some LSPs will be installed per default

--- init.lua
+++ init.lua
@@ -520,7 +520,29 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'eslint-lsp',
+        'goimports',
+        'gopls',
+        'gotests',
+        'gotestsum',
+        'gradle-language-server',
+        'groovy-language-server',
+        'jdtls',
+        'js-debug-adapter',
+        'json-lsp',
+        'kotlin-language-server',
+        'ktlint',
+        'lua-language-server',
+        'marksman',
+        'nixpkgs-fmt',
+        'prettier',
+        'prettierd',
+        'rnix-lsp',
+        'shfmt',
+        'stylua',
+        'typescript-language-server',
+        'vtsls',
+        'yaml-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
