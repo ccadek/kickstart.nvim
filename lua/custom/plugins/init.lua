2157732e45b5bbd3c73b7145489519b206f7c460
Chris <c.cadek@mailbox.org>
2024-06-18 00:37:59 +0200

    enables autopair functionality

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,10 +1,12 @@
 return {
+  require 'custom.plugins.autopairs',
   require 'custom.plugins.vim-sleuth',
   require 'custom.plugins.comment.comment',
   require 'custom.plugins.git.git-blame',
   require 'custom.plugins.oil',
   require 'custom.plugins.git.gitsigns',
   require 'custom.plugins.which-key',
+  require 'custom.plugins.keybindings',
   require 'custom.plugins.telescope',
   require 'custom.plugins.lsp.lspconfig',
   require 'custom.plugins.lsp.conform',
