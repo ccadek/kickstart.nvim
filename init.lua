43a0d9660528bdca3ff12010b861fe3c4f451b6b
Chris Cadek <cc@wh-i.at>
2024-06-17 15:22:38 +0200

    modfies oil.nvim config

--- init.lua
+++ init.lua
@@ -166,13 +166,25 @@ require('lazy').setup({
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
       require('oil').setup {
+        default_file_explorer = true,
         columns = { 'icon' },
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
         keymaps = {
           ['<C-h>'] = false,
           ['<M-h>'] = 'actions.select_split',
+          ['g.'] = 'actions.toggle_hidden',
+          [':q'] = 'actions.close',
         },
         view_options = {
           show_hidden = true,
+          is_always_hidden = function(name, bufnr)
+            -- always hide .git, .idea and node_modules folder
+            if name == '.git' or name == '.idea' or name == 'node_modules' then
+              return true
+            end
+            return false
+          end,
         },
       }
 
