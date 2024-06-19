3faa955a95ecbbc8ada8bea87208326e6ded6bf0
Christopher Cadek <cc@wh-i.at>
2024-06-19 15:23:51 +0200

    telescope should ignore node_modules and .git directories

--- lua/custom/plugins/telescope/telescope.lua
+++ lua/custom/plugins/telescope/telescope.lua
@@ -54,6 +54,12 @@ return {
       --   },
       -- },
       -- pickers = {}
+      defaults = {
+        file_ignore_patterns = {
+          'node_modules',
+          '.git',
+        },
+      },
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
